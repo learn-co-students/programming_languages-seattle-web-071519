2f84e69a9ecac59565254bee7559ace4a2878acb
ayerest <irisiseve@gmail.com>
2019-08-12 15:10:27 -0700

    finished programming languages lab

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,28 @@
+
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |style, language_hash|
+    language_hash.each do |language, type_hash|
+      type_hash.each do |key, value|
+        # binding.pry
+        if !new_hash[language]
+          new_hash[language] = {}
+        end
+        if !new_hash[language][key]
+          new_hash[language][key] = value
+        end
+        # new_hash[language][:style] = []
+        if !new_hash[language][:style]
+          new_hash[language][:style] = []
+          new_hash[language][:style] << style
+        else
+          new_hash[language][:style] << style
+        end
+      end
+    end
+  end
+  new_hash
 end
